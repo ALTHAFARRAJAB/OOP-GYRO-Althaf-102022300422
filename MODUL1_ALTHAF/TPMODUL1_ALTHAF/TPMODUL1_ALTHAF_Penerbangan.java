007973b748634e79a4d07c888b3d5b7909f78702
Muhammad Althaf Arrajab <Malthafarrajab@gmail.com>
2024-10-14 00:55:44 +0700

    MODUL1 UPDATE

--- MODUL1_ALTHAF/TPMODUL1_ALTHAF/TPMODUL1_ALTHAF_Penerbangan.java
+++ MODUL1_ALTHAF/TPMODUL1_ALTHAF/TPMODUL1_ALTHAF_Penerbangan.java
@@ -1,3 +1,51 @@
 public class TPMODUL1_ALTHAF_Penerbangan {
+        private String nomorPenerbangan;
+        private String bandaraKeberangkatan;
+        private String bandaraTujuan;
+        private String waktuKeberangkatan;
+        private String waktuKedatangan;
+        private float hargaTiket;
+        
+        public TPMODUL1_ALTHAF_Penerbangan (String nomorPenerbangan, String bandaraKeberangkatan, String bandaraTujuan, 
+               String waktuKeberangkatan, String waktuKedatangan, float hargaTiket) {
+            this.nomorPenerbangan = nomorPenerbangan;
+            this.bandaraKeberangkatan = bandaraKeberangkatan;
+            this.bandaraTujuan = bandaraTujuan;
+            this.waktuKeberangkatan = waktuKeberangkatan;
+            this.waktuKedatangan = waktuKedatangan;
+            this.hargaTiket = hargaTiket;
+        }
+    
+        public String getNomorPenerbangan() {
+            return nomorPenerbangan;
+        }
+    
+        public String getBandaraKeberangkatan() {
+            return bandaraKeberangkatan;
+        }
+    
+        public String getBandaraTujuan() {
+            return bandaraTujuan;
+        }
+    
+        public String getWaktuKeberangkatan() {
+            return waktuKeberangkatan;
+        }
+    
+        public String getWaktuKedatangan() {
+            return waktuKedatangan;
+        }
+    
+        public float getHargaTiket() {
+            return hargaTiket;
+        }
+    
+        public void tampilDaftarPenerbangan(int index) {
+            System.out.println(index + ". ");
+            System.out.println("Nomor Penerbangan: " + nomorPenerbangan);
+            System.out.println("Bandara Keberangkatan: " + bandaraKeberangkatan + " ---> Bandara Tujuan: " + bandaraTujuan);
+            System.out.println("Waktu Keberangkatan: " + waktuKeberangkatan + " ---> Waktu Kedatangan: " + waktuKedatangan);
+            System.out.println("Harga Tiket: Rp." + hargaTiket + "\n");
+        }
+    }
 
-}
